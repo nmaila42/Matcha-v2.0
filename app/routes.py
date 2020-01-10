954eda8bcba5deb9d88c5b46714cca352c0f42c5
Mosima Damentt MAMALEKA <mmamalek@c4r5s2.wethinkcode.co.za>
2020-01-10 08:58:49 +0200

    switching work station

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,15 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username':'Mosima'}
     return render_template('index.html', title = 'Welcome', user = user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user = {'username':'Mosima'}
+    print(request.form["username"])
+    return render_template('welcome.html', title = 'logger', user = user)
 
