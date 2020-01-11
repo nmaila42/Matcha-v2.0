e5d79c416fd0bb4f6dff925aee5b7408d6b690b1
Mosima Damentt MAMALEKA <mmamalek@c5r5s11.wethinkcode.co.za>
2020-01-11 17:59:03 +0200

    sockets

--- sockets/static/js/script.js
+++ sockets/static/js/script.js
@@ -8,6 +8,8 @@ socket.on('connect', function() {
     socket.emit('connection-event', {data: 'I\'m connected!'}, function(x){sender = x});
 });
 
+window.onloadstart = getCookie;
+getCookie();
 window.onbeforeunload = closeConnection
 
 function closeConnection(){
@@ -44,3 +46,39 @@ socket.on('message-recieved', function(msg){
 
     console.log(msg);
 })
+
+
+function setCookie(cookieName, cookieValue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    
+    var user = getCookie("username");
+
+    while (user == "" || user == null) {
+      user = prompt("Please enter your name:", "");
+      if (user != "" && user != null) {
+        setCookie("username", user, 365);
+        break;
+      }
+    }
+  }
